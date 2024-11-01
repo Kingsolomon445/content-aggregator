18a52c1c753874b3db861fa87f12b2ba6a066d60
Kingsolomon445 <youngfadd@gmail.com>
2024-11-01 18:41:45 -0400

    testing

--- content_aggregator/celery.py
+++ content_aggregator/celery.py
@@ -1,16 +1,10 @@
 import os
-import ssl
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'content_aggregator.settings')
 
-
+import ssl
 from django.conf import settings
 from celery import Celery
-# from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
-# Set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'content_aggregator.settings')
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
 
 app = Celery('content_aggregator')
 
@@ -21,8 +15,10 @@ app = Celery('content_aggregator')
 app.config_from_object(f'django.conf:settings', namespace='CELERY')
 
 
-app.conf.broker_url = os.environ.get('REDIS_TLS_URL', 'redis://localhost:6379/0')
-app.conf.result_backend = os.environ.get('REDIS_TLS_URL', 'redis://localhost:6379/0')
+# app.conf.broker_url = os.environ.get('REDIS_TLS_URL', 'redis://localhost:6379/0')
+# app.conf.result_backend = os.environ.get('REDIS_TLS_URL', 'redis://localhost:6379/0')
+app.conf.broker_url = 'rediss://:p98d90061dcbe89e6985697a5415dcf5d633e0ee0438679dc151aa464e3cab1b4@ec2-18-206-36-186.compute-1.amazonaws.com:20450'
+app.conf.result_backend = 'rediss://:p98d90061dcbe89e6985697a5415dcf5d633e0ee0438679dc151aa464e3cab1b4@ec2-18-206-36-186.compute-1.amazonaws.com:20450'
 
 app.conf.broker_transport_options = {
     'ssl_cert_reqs': 'CERT_NONE'  # or CERT_OPTIONAL, CERT_REQUIRED
